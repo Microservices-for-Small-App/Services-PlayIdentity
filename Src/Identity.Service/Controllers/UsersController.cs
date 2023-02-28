59b409e346a49676ca633bda2c6ac98825f5b274
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-02-28 23:32:39 +0530

    Update UsersController.cs

--- Src/Identity.Service/Controllers/UsersController.cs
+++ Src/Identity.Service/Controllers/UsersController.cs
@@ -17,13 +17,62 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
-    public ActionResult<IEnumerable<UserDto>> Get()
+    public ActionResult<IReadOnlyCollection<UserDto>> Get()
     {
         var users = _userManager.Users
             .ToList()
             .Select(user => user.AsDto());
 
         return Ok(users);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<UserDto>> GetByIdAsync(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user.AsDto();
+    }
+
+    // /users/{123}
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutAsync(Guid id, UpdateUserDto userDto)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        user.Email = userDto.Email;
+        user.UserName = userDto.Email;
+        user.Gil = userDto.Gil;
+
+        await _userManager.UpdateAsync(user);
+
+        return NoContent();
+    }
+
+    // /users/{123}
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id)
+    {
+        var user = await _userManager.FindByIdAsync(id.ToString());
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        await _userManager.DeleteAsync(user);
+
+        return NoContent();
     }
 
 }
