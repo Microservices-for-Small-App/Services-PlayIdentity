7eb8aa5d7e1b1bbb086e85d971c6300f594ee541
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-03 16:09:25 +0530

    Swamy/03mar work (#5)
    * Update appsettings.json
    
    * Added Scope

--- Src/Identity.Service/Settings/IdentityServerSettings.cs
+++ Src/Identity.Service/Settings/IdentityServerSettings.cs
@@ -4,7 +4,9 @@ namespace Identity.Service.Settings;
 
 public class IdentityServerSettings
 {
-    public IReadOnlyCollection<ApiScope> ApiScopes { get; init; } = Array.Empty<ApiScope>();
+    public IReadOnlyCollection<ApiScope>? ApiScopes { get; init; }
+
+    public IReadOnlyCollection<ApiResource>? ApiResources { get; init; }
 
     public IReadOnlyCollection<Client>? Clients { get; init; }
 
