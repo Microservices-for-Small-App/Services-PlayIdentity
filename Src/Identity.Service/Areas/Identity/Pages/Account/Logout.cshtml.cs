2c1c98be3b59da455dbd9bd57bbf5b07b2bb7c2b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-16 17:38:41 +0530

    Logout

--- Src/Identity.Service/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Src/Identity.Service/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -2,34 +2,48 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+using Duende.IdentityServer.Services;
+using Identity.Service.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
 namespace Identity.Service.Areas.Identity.Pages.Account
 {
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly IIdentityServerInteractionService _interaction;
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        public LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger
+            , IIdentityServerInteractionService interaction)
         {
             _signInManager = signInManager;
             _logger = logger;
+            _interaction = interaction;
+        }
+
+        public async Task<IActionResult> OnGet(string logoutId)
+        {
+            var context = await _interaction.GetLogoutContextAsync(logoutId);
+            if (context?.ShowSignoutPrompt == false)
+            {
+                return await this.OnPost(context.PostLogoutRedirectUri);
+            }
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPost(string returnUrl = null)
         {
             await _signInManager.SignOutAsync();
             _logger.LogInformation("User logged out.");
+
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                // return LocalRedirect(returnUrl);
+                return Redirect(returnUrl);
             }
             else
             {
