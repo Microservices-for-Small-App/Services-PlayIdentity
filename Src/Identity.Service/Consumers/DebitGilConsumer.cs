0f10581867b9d6f1d7f4516098d139e45758f99c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-22 16:22:06 +0530

    Consumer

---
+++ Src/Identity.Service/Consumers/DebitGilConsumer.cs
@@ -0,0 +1,39 @@
+﻿using Identity.Contracts;
+using Identity.Service.Entities;
+using MassTransit;
+using Microsoft.AspNetCore.Identity;
+
+namespace Identity.Service.Consumers;
+
+public class DebitGilConsumer : IConsumer<DebitGil>
+{
+    private readonly UserManager<ApplicationUser> userManager;
+
+    public DebitGilConsumer(UserManager<ApplicationUser> userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public async Task Consume(ConsumeContext<DebitGil> context)
+    {
+        var message = context.Message;
+
+        var user = await userManager.FindByIdAsync(message.UserId.ToString());
+
+        if (user == null)
+        {
+            throw new UnknownUserException(message.UserId);
+        }
+
+        user.Gil -= message.Gil;
+
+        if (user.Gil < 0)
+        {
+            throw new InsufficientFundsException(message.UserId, message.Gil);
+        }
+
+        await userManager.UpdateAsync(user);
+
+        await context.Publish(new GilDebited(message.CorrelationId));
+    }
+}
---
+++ Src/Identity.Service/Exceptions/InsufficientFundsException.cs
@@ -0,0 +1,17 @@
+﻿namespace Identity.Service.Exceptions;
+
+[Serializable]
+internal class InsufficientFundsException : Exception
+{
+    public InsufficientFundsException(Guid userId, decimal gilToDebit) : base($"Not enough gil to debit {gilToDebit} from user '{userId}'")
+    {
+        UserId = userId;
+
+        GilToDebit = gilToDebit;
+    }
+
+    public Guid UserId { get; }
+
+    public decimal GilToDebit { get; }
+}
+
